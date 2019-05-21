81a302cc54303d80cc54207bbf11d45f0155a7a6
Jesus Rey Mejías <jesusrey85@gmail.com>
2019-05-21 09:42:14 +0200

    misc

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
 import App from './App';
-import VueI18n from 'vue-i18n'
 
 import BootstrapVue from 'bootstrap-vue';
 import 'bootstrap/dist/css/bootstrap.css';
