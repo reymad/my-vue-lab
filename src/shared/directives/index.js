14e04fcf1cd82d6141f0bf7279fd3bf36f3f37e5
Jesus Rey Mejías <jesusrey85@gmail.com>
2019-05-30 12:28:37 +0200

    fix

--- src/shared/directives/index.js
+++ src/shared/directives/index.js
@@ -1,10 +1,16 @@
 // https://vuejs.org/v2/guide/custom-directive.html
 
+import Vue from 'vue'
+
 // Register a global custom directive called `v-focus`
 Vue.directive('focus', {
     // When the bound element is inserted into the DOM...
     inserted: function (el) {
       // Focus the element
-      el.focus()
+     // Vue.nextTick(function() {
+     //   el.focus();
+     // });
+     setTimeout( () => { el.focus(); console.log("Directive loaded but not focused...") } );
+     
     }
   })
